d2c962b0ebb3c8a73724e934faa1249150eff3a7
Tine Mezgec <tine.mezgec@gmail.com>
2016-09-05 21:04:12 +0200

    initial commit

---
+++ watch_idle.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+username=$(who|grep guest| tr -s " "| cut -d" " -f1)
+display=$(who|grep guest| tr -s " "| cut -d" " -f6| tr -d "()")
+export DISPLAY=$display
+
+idle_limit=$(expr 5 \* 60 \* 1000)
+
+while true; do
+    idletime=$(sudo -u $username xprintidle)
+    echo $idletime
+    if [ "$idletime" -gt "$idle_limit" ]; then
+	echo "idle time reached, restarting lightdm";
+	/etc/init.d/lightdm restart
+	sleep 1
+	username=""
+	while [ -z $username ]; do
+	    username=$(who|grep guest| tr -s " "| cut -d" " -f1)
+	    sleep 1
+	done
+	display=$(who|grep guest| tr -s " "| cut -d" " -f6| tr -d "()")
+	echo "got user $username at $display"
+	export DISPLAY=$display
+	echo "waiting for ui init"
+	lastidle=$(sudo -u $username xprintidle)
+	sleep 1
+	while [ "$lastidle" -lt "$(sudo -u $username xprintidle)" ]; do
+	    echo $lastidle
+	    lastidle=$(sudo -u $username xprintidle)
+	    sleep 1
+	done
+	echo "ui init finished, waiting for activity"
+	lastidle=$(sudo -u $username xprintidle)
+	sleep 1
+	#$(sudo -u $username firefox)
+	while [ "$lastidle" -lt "$(sudo -u $username xprintidle)" ]; do
+	    echo $lastidle
+	    lastidle=$(sudo -u $username xprintidle)
+	    sleep 1
+	done
+	echo "activity!, back to main loop"
+    fi
+    sleep 1
+done
