5306b130bb70c369a4404141c8c186f62ff83eac
Tine Mezgec <tine.mezgec@gmail.com>
2016-09-06 00:16:19 +0200

    shortened the sleeps that happen after ui restart

--- watch_idle.sh
+++ watch_idle.sh
@@ -15,31 +15,31 @@ while true; do
     if [ "$idletime" -gt "$idle_limit" ]; then
 	echo "idle time reached, restarting lightdm";
 	/etc/init.d/lightdm restart
-	sleep 10
+	sleep 1
 	username=""
 	while [ -z $username ]; do
 	    username=$(who|grep guest| tr -s " "| cut -d" " -f1)
-	    sleep 10
+	    sleep 1
 	done
 	display=$(who|grep guest| tr -s " "| cut -d" " -f5| tr -d "()")
 	echo "got user $username at $display"
 	export DISPLAY=$display
 	echo "waiting for ui init"
 	lastidle=$(sudo -u $username xprintidle)
-	sleep 10
+	sleep 1
 	while [ "$lastidle" -lt "$(sudo -u $username xprintidle)" ]; do
 	    echo $lastidle
 	    lastidle=$(sudo -u $username xprintidle)
-	    sleep 10
+	    sleep 1
 	done
 	echo "ui init finished, waiting for activity"
 	lastidle=$(sudo -u $username xprintidle)
-	sleep 10
+	sleep 1
 	#$(sudo -u $username firefox)
 	while [ "$lastidle" -lt "$(sudo -u $username xprintidle)" ]; do
 	    echo $lastidle
 	    lastidle=$(sudo -u $username xprintidle)
-	    sleep 10
+	    sleep 1
 	done
 	echo "activity!, back to main loop"
     fi
