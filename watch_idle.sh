9be961f0e1b51228b2464d59609f7a588f0f2598
Tine Mezgec <tine.mezgec@gmail.com>
2016-09-06 00:05:51 +0200

    fixed bug where scrip didnt work when stgarted at boot and raised wait times to 10s

--- watch_idle.sh
+++ watch_idle.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-username=$(who|grep guest| tr -s " "| cut -d" " -f1)
-display=$(who|grep guest| tr -s " "| cut -d" " -f6| tr -d "()")
+username=""
+while [ -z $username ]; do
+    username=$(who|grep guest| tr -s " "| cut -d" " -f1)
+    sleep 10
+done
+display=$(who|grep guest| tr -s " "| cut -d" " -f5| tr -d "()")
 export DISPLAY=$display
 
 idle_limit=$(expr 5 \* 60 \* 1000)
@@ -11,33 +15,33 @@ while true; do
     if [ "$idletime" -gt "$idle_limit" ]; then
 	echo "idle time reached, restarting lightdm";
 	/etc/init.d/lightdm restart
-	sleep 1
+	sleep 10
 	username=""
 	while [ -z $username ]; do
 	    username=$(who|grep guest| tr -s " "| cut -d" " -f1)
-	    sleep 1
+	    sleep 10
 	done
-	display=$(who|grep guest| tr -s " "| cut -d" " -f6| tr -d "()")
+	display=$(who|grep guest| tr -s " "| cut -d" " -f5| tr -d "()")
 	echo "got user $username at $display"
 	export DISPLAY=$display
 	echo "waiting for ui init"
 	lastidle=$(sudo -u $username xprintidle)
-	sleep 1
+	sleep 10
 	while [ "$lastidle" -lt "$(sudo -u $username xprintidle)" ]; do
 	    echo $lastidle
 	    lastidle=$(sudo -u $username xprintidle)
-	    sleep 1
+	    sleep 10
 	done
 	echo "ui init finished, waiting for activity"
 	lastidle=$(sudo -u $username xprintidle)
-	sleep 1
+	sleep 10
 	#$(sudo -u $username firefox)
 	while [ "$lastidle" -lt "$(sudo -u $username xprintidle)" ]; do
 	    echo $lastidle
 	    lastidle=$(sudo -u $username xprintidle)
-	    sleep 1
+	    sleep 10
 	done
 	echo "activity!, back to main loop"
     fi
-    sleep 1
+    sleep 10
 done
